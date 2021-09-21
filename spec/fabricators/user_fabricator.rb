ddaa9e8af9cca18e0bcbfcde3f7079321a152ec3
Xavier MALPARTY <xavier@nimblehq.co>
2021-09-21 21:08:41 +0700

    [#3] Fix request test with authentication

--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 Fabricator(:user) do
-  first_name { FFaker::Name.last_name }
+  last_name { FFaker::Name.last_name }
   first_name { FFaker::Name.first_name }
-  nickname { "#{first_name}.#{last_name}" }
   email { FFaker::Internet.email }
+  nickname { FFaker::Internet.user_name }
   password 'password123'
   password_confirmation 'password123'
 end
--- spec/requests/articles_controller_spec.rb
+++ spec/requests/articles_controller_spec.rb
@@ -6,9 +6,19 @@ describe ArticlesController, type: :request do
   describe 'GET #index' do
     context 'given no parameter' do
       it 'returns a 200 status code' do
+        sign_in Fabricate(:user)
+
         get :index
 
         expect(response.status).to eq(200)
+      end
+    end
+
+    context 'when the user is not authenticated' do
+      it 'redirects to the login page' do
+        get :index
+
+        expect(response).to eq(200)
       end
     end
   end
