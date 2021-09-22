708a690a8d5554cf0b38d0ff6346ab853c631905
Xavier MALPARTY <xavier@nimblehq.co>
2021-09-22 12:08:14 +0700

    [#3] Move posts-controller tests to articles-controller tests

--- spec/requests/articles_controller_spec.rb
+++ spec/requests/articles_controller_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-describe PostsController, type: :request do
+describe ArticlesController, type: :request do
   describe 'GET #index' do
     context 'given no parameter' do
       it 'returns a 200 status code' do
