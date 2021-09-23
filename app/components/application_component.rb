8fdffa9ea5725bc767f6347962a79a2c51d54103
rafayet-monon <rafayet.monon@gmail.com>
2021-09-24 00:55:27 +0600

    Add components

---
+++ app/components/application_component.rb
@@ -0,0 +1,11 @@
+class ApplicationComponent < ViewComponent::Base
+  def initialize(tag: nil, classes: nil, **options)
+    @tag = tag
+    @classes = classes
+    @options = options
+  end
+
+  def call
+    content_tag(@tag, content, class: @classes, **@options) if @tag
+  end
+end
---
+++ app/components/base/button/component.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+module Base
+  module Button
+    class Component < ApplicationComponent
+
+      attr_reader :type, :link
+
+      BASE_CLASSES = %w[btn].freeze
+
+      PRIMARY_CLASSES = %w[btn-primary].freeze
+      SECONDARY_CLASSES = %w[btn-secondary].freeze
+      SUCCESS_CLASSES = %w[btn-success].freeze
+      DANGER_CLASSES = %w[btn-danger].freeze
+      LINK_CLASSES = %w[btn-link].freeze
+
+      BUTTON_TYPE_MAPPING = {
+        primary: PRIMARY_CLASSES,
+        secondary: SECONDARY_CLASSES,
+        success: SUCCESS_CLASSES,
+        danger: DANGER_CLASSES,
+        link: LINK_CLASSES
+      }.freeze
+
+      def initialize(type: :primary, link: '#', **options)
+        @options = options
+
+        @options[:tag] ||= :a
+        @options[:href] = link
+        @options[:classes] = classes(type)
+      end
+
+      def call
+        render ApplicationComponent.new(**@options).with_content(content)
+      end
+
+      private
+
+      def classes(type)
+        (BASE_CLASSES + BUTTON_TYPE_MAPPING[type]).join(' ')
+      end
+    end
+  end
+end
