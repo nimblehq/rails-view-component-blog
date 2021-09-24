203e759f21acc98508ea7e40fe4a83413b978bf2
Xavier MALPARTY <xavier@nimblehq.co>
2021-09-24 16:07:14 +0700

    Add more tests for the button component

--- spec/components/base/button/component_spec.rb
+++ spec/components/base/button/component_spec.rb
@@ -20,4 +20,22 @@ RSpec.describe ::Base::Button::Component, type: :component do
       expect(button).to have_selector('a.btn-primary')
     end
   end
+
+  context 'given all params and content' do
+    it 'renders a link that includes the content' do
+      content = 'Click me!'
+
+      button = render_inline(described_class.new(type: :link)) { content }
+
+      expect(button).to have_text(content)
+    end
+
+    it 'renders a link with the selected type btn class' do
+      content = 'Click me!'
+
+      button = render_inline(described_class.new(type: :link)) { content }
+
+      expect(button).to have_selector('a.btn-link')
+    end
+  end
 end
