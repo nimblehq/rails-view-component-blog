82e5b3bffe6fb99e73302b673fa034525c1b0d74
rafayet-monon <rafayet.monon@gmail.com>
2021-09-24 15:32:25 +0600

    add links to preview and story

--- spec/components/articles/list_items/component_spec.rb
+++ spec/components/articles/list_items/component_spec.rb
@@ -5,13 +5,13 @@ require 'rails_helper'
 RSpec.describe Articles::ListItems::Component, type: :component do
   context 'given a collection of articles' do
     it 'renders the article list' do
-      article_1 = Fabricate(:article)
-      article_2 = Fabricate(:article)
+      article_one = Fabricate(:article)
+      article_two = Fabricate(:article)
 
       article_lists = render_inline(described_class.with_collection(Article.all))
 
-      expect(article_lists).to have_link(article_1.title, href: article_path(article_1))
-      expect(article_lists).to have_link(article_2.title, href: article_path(article_2))
+      expect(article_lists).to have_link(article_1.title, href: article_path(article_one))
+      expect(article_lists).to have_link(article_2.title, href: article_path(article_two))
     end
   end
 end
--- spec/components/posting_details/component_spec.rb
+++ spec/components/posting_details/component_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe PostingDetails::Component, type: :component do
   context 'given name and time' do
     it 'renders posting details component' do
       name = 'Author'
-      time = Time.new(2021, 9, 21)
+      time = Time.zone.local(2021, 9, 21)
 
       details = render_inline(described_class.new(name: name, time: time))
 
